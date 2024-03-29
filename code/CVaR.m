4a787302036dd13c8f20cd489f4042144e8b2f5c
Sukhorskyy <78483500+Sukhorskyy@users.noreply.github.com>
2024-03-29 20:12:36 +0200

    Added CVaR.m

---
+++ code/CVaR.m
@@ -0,0 +1,19 @@
+function w = CVaR(benchmark, factors, B, beta)
+    fun = @(theta) objectivefunc((-1)*(benchmark + factors * theta), beta);
+    options = optimoptions('fmincon', 'Display', 'off', 'Algorithm', 'sqp');
+    nf = size(factors, 2);
+    X0  = 0 * ones(nf, 1); 
+    A   = ones(1, nf);
+    Aeq = [];
+    Beq = [];
+    LB  = zeros(1, nf);
+    UB  = Inf(1, nf);
+    w = fmincon(fun, X0, A, B, Aeq, Beq, LB, UB, [], options);
+end
+
+function CVaR_value = objectivefunc(returns, beta)
+    sorted = sort(returns);
+    n = numel(sorted);
+    index = floor((1-beta) * n);
+    CVaR_value = mean(sorted(1:index));
+end
